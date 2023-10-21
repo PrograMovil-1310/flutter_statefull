3381aa51a56cb899fccb7867b4198f8bcf9eaac9
Ivan Cruz <ivancruzhn@gmail.com>
2023-10-21 11:26:31 -0600

    funcionalidad de contador

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -6,17 +6,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePage extends State<HomePage> {
+  int valor = 0;
   @override
   build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text("Contador"),
+        appBar: AppBar(
+          title: Center(
+            child: Text("Contador"),
+          ),
         ),
-      ),
-      body: Center(
-        child: Text('Hello World!'),
-      ),
-    );
+        body: Center(
+          child: Text(
+            'Numero: $valor',
+            style: TextStyle(fontSize: 40.0),
+          ),
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FloatingActionButton(
+              child: Icon(Icons.remove),
+              onPressed: () => setState(() => valor--),
+            ),
+            SizedBox(width: 20),
+            FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () => setState(() => valor++),
+            ),
+             SizedBox(width: 20),
+            FloatingActionButton(
+              child: Icon(Icons.restore),
+              onPressed: () => setState(() => valor=0),
+            ),
+          ],
+        ));
   }
 }
